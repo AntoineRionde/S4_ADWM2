e769fe96b063c8b3cd6b30a1b2640652354fa6b7
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-14 15:45:01 +0200

    Création actions

---
+++ minipress.core/press.appli/src/actions/AbstractAction.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace press\app\actions;
+
+
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+abstract class AbstractAction
+{
+    abstract public function __invoke(Request $request, Response $response, array $args) : Response;
+
+}
