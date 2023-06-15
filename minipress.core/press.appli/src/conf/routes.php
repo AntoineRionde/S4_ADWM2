b64f05e82017a4320818b714227a5d570c4fb2f2
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-06-15 15:23:27 +0200

    Suppression import

--- minipress.core/press.appli/src/conf/routes.php
+++ minipress.core/press.appli/src/conf/routes.php
@@ -6,15 +6,8 @@ use press\app\actions\GetArticleAction;
 use press\app\actions\GetCategoriesAction;
 use press\app\actions\GetHomeAction;
 use Slim\App;
-use Slim\Psr7\Response as Response;
-use Slim\Psr7\Request as Request;
 
 return function (App $app): void {
-    /**
-    $app->get('/', function (Request $request, Response $response, array $args) {
-        $response->getBody()->write("Salut à tous!");
-        return $response;
-    });*/
 
     $app->get('/', GetHomeAction::class)->setName('home');
     $app->get('/articles[/]', GetArticleAction::class)->setName("articles");
