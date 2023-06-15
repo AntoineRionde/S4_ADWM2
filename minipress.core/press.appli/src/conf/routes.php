85cbef4909dcc408600a2bff23776c6fdf30d88c
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-15 14:42:30 +0200

    Modifie page d'accueil

--- minipress.core/press.appli/src/conf/routes.php
+++ minipress.core/press.appli/src/conf/routes.php
@@ -1,17 +1,19 @@
 <?php
 
-
 use press\app\actions\GetArticleAction;
 use press\app\actions\GetCategoriesAction;
+use press\app\actions\GetHomeAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (\Slim\App $app): void {
+    /**
     $app->get('/', function (Request $request, Response $response, array $args) {
-        $response->getBody()->write("Hello world!");
+        $response->getBody()->write("Salut à tous!");
         return $response;
-    });
+    });*/
 
+    $app->get('/', GetHomeAction::class)->setName('home');
     $app->get('/articles[/]', GetArticleAction::class)->setName("articles");
     $app->get('/categories[/]', GetCategoriesAction::class)->setName("categories");
     
