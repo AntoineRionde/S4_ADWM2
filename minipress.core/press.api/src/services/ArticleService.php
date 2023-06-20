c9816c2ce329a5118661d56b5c0094eede5a1511
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-06-20 16:48:34 +0200

    Ajout de méthodes ArticleService

--- minipress.core/press.api/src/services/ArticleService.php
+++ minipress.core/press.api/src/services/ArticleService.php
@@ -8,112 +8,31 @@ use press\api\models\Article;
 
 class ArticleService
 {
-
-    /**
-     * Méthode permettant de récupérer tous les articles
-     * @return array $articles
-     */
-    function getArticles(): array
-    {
-        $articles = Article::all();
-        return $articles->toArray();
-    }
-
-
-    /**
-     * Méthode permettant de récupérer un article par son id
-     * @param int $id
-     * @return array $article
-     * @throws Exception $e
-     */
-    function getArticleById(int $id): array
+    public function getPublishedArticles() : array
     {
         try {
-            return Article::findOrFail($id)->toArray();
-        } catch (\Exception $e) {
-            throw new \Exception("L'id de l'article n'est pas renseigné");
+            return Article::where('date_publication', '<=', date_create()->format('Y-m-d H:i:s'))->get()->toArray();
+        } catch (ModelNotFoundException $e) {
+            throw new Exception("Aucun article n'est publié");
         }
     }
 
-    /**
-     * Méthode permettant de créer un article
-     * @param array $data
-     * @return array $article
-     */
-    function createArticle(array &$data): array
-    {
-        $article = new Article();
-        $article->titre = $data['titre'];
-        $article->date_creation = date_create()->format('Y-m-d H:i:s');
-        $article->auteur = $data['auteur'];
-        $article->resume = $data['resume'];
-        $article->contenu = $data['contenu'];
-        $article->date_publication = date_create()->format('Y-m-d H:i:s');
-
-        if ($data['image'] === '') {
-            $data['image'] = null;
-        }
-        $article->image = $data['image'];
-
-        $article->cat_id = $data['cat_id'];
-        $article->save();
-        return $article->toArray();
-    }
-
-    /**
-     * Méthode permettant de supprimer un article
-     * @param int $idArt
-     * @return array $article
-     */
-    function deleteArticle(int $idArt): array
+    public function getArticlesPublishedByCategorieId(int $cat_id) : array
     {
         try {
-            $article = Article::findOrFail($idArt);
-            $article->delete();
-            return $article->toArray();
-        } catch (\Exception $e) {
-            throw new \Exception("L'id de l'article n'est pas renseigné");
-        }
-    }
-
-    /**
-     * Méthode permettant de mettre à jour un article
-     * @param int $idArt
-     * @param array $data
-     * @return array $article
-     */
-    function updateArticle(int $idArt, array $data): array
-    {
-        try {
-            $article = Article::findOrFail($idArt);
-            $article->titre = $data['titre'];
-            $article->contenu = $data['contenu'];
-            $article->cat_id = $data['idCateg'];
-            $article->save();
-            return $article->toArray();
-        } catch (\Exception $e) {
-            throw new \Exception("L'id de l'article n'est pas renseigné");
-        }
-    }
-
-    /**
-     * Méthode permettant de récupérer les articles d'une catégorie
-     * @param int $id id de la catégorie
-     * @return array $articles
-     * @throws Exception $e
-     */
-    public function getArticlesByCategorieId($id): array
-    {
-        try {
-            return Article::where('cat_id', $id)->get()->toArray();
+            return Article::where('cat_id', $cat_id)->where('date_publication', '<=', date_create()->format('Y-m-d H:i:s'))->get()->toArray();
         } catch (ModelNotFoundException $e) {
             throw new Exception("L'id de la catégorie n'est pas renseigné");
         }
     }
 
-    public function getPublishedArticles()
+    public function getArticlePublishedById(int $id_a) : array
     {
-        return Article::where('date_publication', '<=', date_create()->format('Y-m-d H:i:s'))->get()->toArray();
+        try {
+            return Article::where('id', $id_a)->where('date_publication', '<=', date_create()->format('Y-m-d H:i:s'))->firstOrFail()->toArray();
+        } catch (ModelNotFoundException $e) {
+            throw new Exception("L'id de l'article n'est pas renseigné");
+        }
     }
 
 
