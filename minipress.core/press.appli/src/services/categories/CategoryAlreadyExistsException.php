76b0655f65cacde1c0225ef454f04ed7b569b478
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-21 16:33:21 +0200

    Modif action création categorie

---
+++ minipress.core/press.appli/src/services/categories/CategoryAlreadyExistsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace press\app\services\categories;
+
+use Exception;
+use Throwable;
+
+class CategoryAlreadyExistsException extends \Exception
+{
+    public function __construct(string $message = "cette catégorie existe déjà", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
