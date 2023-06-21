517cb6ce46fa78409a88dcc57399080bfbae4cf2
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-06-21 11:49:06 +0200

    Correction Api Auteurs

--- minipress.core/press.api/src/actions/GetApiArticlesByIdAuteur.php
+++ minipress.core/press.api/src/actions/GetApiArticlesByIdAuteur.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace press\app\actions;
+namespace press\api\actions;
 
 use press\api\services\ArticleService;
 use Slim\Psr7\Request as Request;
 use Slim\Psr7\Response as Response;
 
-class GetApiArticlesByIdAuteur
+class GetApiArticlesByIdAuteur extends AbstractAction
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
@@ -14,7 +14,6 @@ class GetApiArticlesByIdAuteur
         $article = $service->getArticlesByIdAuteur($args['id']);
 
         $data = ['article' => $article];
-        echo json_encode($data);
         $response->getBody()->write(json_encode($data));
         return
             $response->withHeader('Content-Type', 'application/json')
